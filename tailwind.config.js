9c8d9f62c0131d2b23bd5bae1caffc8049cdca42
matthcarson01 <matthcarson01@gmail.com>
2020-10-16 14:37:11 -0400

    quick styles for deploy

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+    // defaultLineHeights: true,
+    // standardFontWeights: true
+  },
+  purge: ["src/**/*.html"],
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
